aa87ebd475420ece532e36fcbbd140373951863e
DESKTOP-M46Q808\Alexander <a.nebling@live.de>
2017-09-17 12:04:40 +0200

    Update 0.2
    * Buttons open some Webpages (for test)
    
    * Play Button work if exe in the same folder
    
    *Picture Error fix

--- ProjectFury_Launcher/src/projectfury_launcher/FXMLDocumentController.java
+++ ProjectFury_Launcher/src/projectfury_launcher/FXMLDocumentController.java
@@ -5,11 +5,15 @@
  */
 package projectfury_launcher;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
@@ -23,7 +27,122 @@ public class FXMLDocumentController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        engine = wv_main.getEngine();
+    }
+    
+    @FXML
+    WebView wv_main;
+   
+  
+   private WebEngine engine;
+    
+    @FXML
+    Button btnClose;
+    
+    @FXML
+    private void bp_Updates(ActionEvent ev)
+    {
+       if(!wv_main.isVisible())
+       {
+            wv_main.setVisible(true);
+            engine.setJavaScriptEnabled(true);
+            engine.load("http://google.de");
+            btnClose.setVisible(true);
+       }
+       else{
+            engine.setJavaScriptEnabled(true);
+            engine.load("http://google.de");
+       }
         
-    }    
+    }
+    
+    @FXML
+    private void bp_Soldier(ActionEvent ev)
+    {
+       if(!wv_main.isVisible())
+       {
+            wv_main.setVisible(true);
+            engine.setJavaScriptEnabled(true);
+            engine.load("http://youtube.de");
+            btnClose.setVisible(true);
+       }
+       else{
+            engine.setJavaScriptEnabled(true);
+            engine.load("http://youtube.de");
+      
+       }
+    }
+    
+    @FXML
+    private void bp_Play(ActionEvent ev)
+    {
+        try {
+            Runtime.getRuntime().exec("FPSTemplate-Win64-DebugGame.exe");
+            System.exit(0);
+        } catch (IOException ex) {
+            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    @FXML
+    private void bp_Friends(ActionEvent ev)
+    {
+               if(!wv_main.isVisible())
+       {
+            wv_main.setVisible(true);
+            engine.setJavaScriptEnabled(true);
+            engine.load("http://gluonhq.com/products/scene-builder/");
+            btnClose.setVisible(true);
+       }
+       else{
+            engine.setJavaScriptEnabled(true);
+            engine.load("http://gluonhq.com/products/scene-builder/");
+      }
+    }
+    
+    @FXML
+    private void bp_Exit(ActionEvent ev)
+    {
+       System.exit(0);
+    }
+    @FXML
+    private void bp_Donate(ActionEvent ev)
+    {
+        if(!wv_main.isVisible())
+       {
+            wv_main.setVisible(true);
+            engine.setJavaScriptEnabled(true);
+            engine.load("http://docs.oracle.com/javase/8/javafx/get-started-tutorial/jfx-overview.htm#JFXST784");
+            btnClose.setVisible(true);
+       }
+       else{
+            engine.setJavaScriptEnabled(true);
+            engine.load("http://docs.oracle.com/javase/8/javafx/get-started-tutorial/jfx-overview.htm#JFXST784");
+     }
+    }
+    
+    @FXML
+    private void bp_Market(ActionEvent ev)
+    {
+               if(!wv_main.isVisible())
+       {
+            wv_main.setVisible(true);
+            engine.setJavaScriptEnabled(true);
+            engine.load("http://trello.com");
+            btnClose.setVisible(true);
+       }
+       else{
+            engine.setJavaScriptEnabled(true);
+            engine.load("http://trello.com");
+     }
+    }
+    
+    @FXML
+    private void bp_Close(ActionEvent ev)
+    {
+        wv_main.setVisible(false);
+        btnClose.setVisible(false);
+    }
+   
     
 }
